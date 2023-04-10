ecf2cb6c024fe06bf50276d7779529a1094a310c
woodong27 <cdw5548@gmail.com>
2023-04-10 22:59:17 +0900

    04/10/2023

---
+++ DataBase/강의 실습/복습/articles/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render, redirect
+from .models import Article, Comment
+from .forms import ArticleForm, CommentForm
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {'articles': articles}
+    return render(request, 'articles/index.html', context)
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    commentForm=CommentForm()
+    comments=article.comment_set.all()
+    context = {
+        'article': article,
+        'commentForm':commentForm,
+        'comments':comments,
+        }
+    return render(request, 'articles/detail.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.user=request.user
+            article.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+
+    context = {'form': form}
+    return render(request, 'articles/create.html', context)
+
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect('articles:index')
+
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', pk=article.pk)
+    else:
+        form = ArticleForm(instance=article)
+
+    context = {'form': form, 'article': article}
+    return render(request, 'articles/update.html', context)
+
+def comments_create(request, pk):
+    article=Article.objects.get(pk=pk)
+    commentForm=CommentForm(request.POST)
+    if commentForm.is_valid():
+        comment=commentForm.save(commit=False)
+        comment.article=article
+        comment.save()
+    return redirect('articles:detail', article.pk)
+
+def comments_delete(request, article_pk, comment_pk):
+    comment=Comment.objects.get(pk=comment_pk)
+    comment.delete()
+    return redirect('articles:detail', article_pk)
