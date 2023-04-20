b379844f1bc14a043c9faceb2937c4f7f4e3b493
woodong27 <cdw5548@gmail.com>
2023-04-20 17:54:17 +0900

    04/20/2023

--- Javascript/강의/04.20/오프라인.js
+++ Javascript/강의/04.20/오프라인.js
@@ -278,5 +278,6 @@ const temp=arr.map(num => num**2)
 console.log(temp)
 
 //결과물
+//배열 고차함수는 성능은 안좋을 수 있지만 가독성에서 훨씬 좋음
 const ans=arr.filter(num => !(num%2)).map(filtered => filtered**2)
 console.log(ans)
