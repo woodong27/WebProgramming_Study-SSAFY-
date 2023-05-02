f4a047f2a58a890622b85b28a0de1d1a3b741f3a
woodong27 <cdw5548@gmail.com>
2023-05-02 10:51:48 +0900

    05/02/2023

---
+++ Vue/강의/05.02/vue-cli/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="메시지 바꾸기"/>
+    <!-- 3. Component를 사용한다-->
+    <MyComponent/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent.vue'; // 1. 만든 component를 가져온다.
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyComponent, // 2. Component를 등록해준다.
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/강의/05.02/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="border">
+    <h1>This is MyComponent</h1>
+    <!-- 3. MyChild를 사용한다.-->
+    <MyChild/>
+  </div>
+</template>
+
+<!--Child Component를 MyComponent에 추가-->
+
+<script>
+// 1. Child Component를 import(가져온다)
+import MyChild from '@/components/MyChild.vue'
+export default {
+    name: 'MyComponent',
+    // 2. MyChild를 등록한다.
+    components:{
+        MyChild,
+    }
+}
+</script>
+
+<style>
+.border{
+  border:solid;
+}
+</style>
